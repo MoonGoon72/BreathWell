841a671027d33914e30058ae275ecaf95d872464
MoonGoon <moongoon.cnu@gmail.com>
2023-02-19 19:39:58 +0900

    feat: 버튼 및 더미 시간 구현

--- BreathWell/ContentView.swift
+++ BreathWell/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+enum WatchStatus {
+    case watchStart
+    case watchStop
+}
+
 struct ContentView: View {
+    @State var isStartBreath: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("ButtonColor")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                    .frame(height: 70)
+                Text("00:00:00")
+                    .bold()
+                    .font(.system(size: 50))
+                Spacer()
+                    
+                Button {
+                    isStartBreath.toggle()
+                } label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: UIScreen.main.bounds.width * 0.5)
+                        .foregroundColor(Color("BackgroundColor"))
+                        Text(isStartBreath ? "호흡 중지" : "호흡 시작")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
