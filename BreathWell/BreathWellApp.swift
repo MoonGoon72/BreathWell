a900d9a59a2c5051d705927effb392862b449552
MoonGoon <moongoon.cnu@gmail.com>
2023-02-19 19:02:19 +0900

    Initial Commit

---
+++ BreathWell/BreathWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathWellApp.swift
+//  BreathWell
+//
+//  Created by cnu on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct BreathWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
